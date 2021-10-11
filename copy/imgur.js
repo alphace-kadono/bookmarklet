133f01ec7c7f6b61eb87836ff9b0769106697294
alphace-kadono <alphace-kadono@users.noreply.github.com>
2021-10-11 18:20:08 +0900

    imgur 画像 URL のコピー

---
+++ copy/imgur.js
@@ -0,0 +1,37 @@
+javascript:
+(func => {
+  console.log('load jquery');
+  const script = document.createElement('script');
+  script.src = '//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js';
+  script.onload = () => {
+    func(jQuery.noConflict(true));
+  };
+  document.body.appendChild(script);
+})(async ($) => {
+  const _x = STR_XPATH => {
+    const xresult = document.evaluate(STR_XPATH, document, null, XPathResult.ANY_TYPE, null);
+    const xnodes = [];
+    let xres;
+    while (xres = xresult.iterateNext()) {
+      xnodes.push(xres);
+    }
+
+    return xnodes;
+  };
+
+  if (!location.href.match(/https:\/\/imgur\.com/)) {
+    alert('このページでは使用できません🙇');
+    return;
+  }
+
+  const selector = '//img[contains(@class, "image-placeholder")]';
+  const item = $(_x(selector));
+
+  let url = item.attr('src');
+  url = url.replace(/\.png/, '.webp?maxwidth=960');
+
+  navigator.clipboard.writeText(url);
+  alert('URL をコピーしました🆗');
+
+  location.href = url;
+});
